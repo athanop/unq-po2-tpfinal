9da01fe4be36e53d2ec682438a29f0af7af95aa5
athan <elnahue.-@live.com.ar>
2022-05-17 17:55:23 -0300

    .

---
+++ test/ar/edu/unq/po2/tpFinal/MuestrasTestCase.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.po2.tpFinal;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MuestrasTestCase {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
