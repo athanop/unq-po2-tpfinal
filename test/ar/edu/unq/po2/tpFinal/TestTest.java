432f8bd2a31822304aecacddd6e9e21af8f0a5de
Julian <Julian@DESKTOP-PFOCKI6>
2022-05-17 18:08:55 -0300

    Muestras objt

--- test/ar/edu/unq/po2/tpFinal/TestTest.java
+++ test/ar/edu/unq/po2/tpFinal/TestTest.java
@@ -1,13 +1,9 @@
+package ar.edu.unq.po2.tpFinal;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class MuestrasTestCase {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+class TestTest {
 
 	@Test
 	void test() {
