ec95a2d07674fb92f0804a79c194e4fcdf44ef88
Julian <Julian@DESKTOP-PFOCKI6>
2022-05-24 17:29:31 -0300

    MuestraTest

--- src/ar/edu/unq/po2/tpFinal/Muestra.java
+++ src/ar/edu/unq/po2/tpFinal/Muestra.java
@@ -1,5 +1,36 @@
 package ar.edu.unq.po2.tpFinal;
 
+import java.awt.image.BufferedImage;
+
 public class Muestra {
+
+	private BufferedImage fotoVinchuca;
+	private Calificacion especieFotografiada;
+	private Ubicacion ubicacion;
+	private Usuario identificacionDeLaPersona;
+
+	public Muestra(BufferedImage fotoVinchuca, Calificacion especieFotografiada, Ubicacion ubicacion,
+			Usuario identificacionDeLaPersona) {
+		this.fotoVinchuca = fotoVinchuca;
+		this.especieFotografiada = especieFotografiada;
+		this.ubicacion = ubicacion;
+		this.identificacionDeLaPersona = identificacionDeLaPersona;
+	}
+
+	public BufferedImage getFotoVinchuca() {
+		return this.fotoVinchuca;
+	}
+
+	public Calificacion getEspecieFotografiada() {
+		return especieFotografiada;
+	}
+
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public Usuario getIdentificacionDeLaPersona() {
+		return identificacionDeLaPersona;
+	}
 
 }
