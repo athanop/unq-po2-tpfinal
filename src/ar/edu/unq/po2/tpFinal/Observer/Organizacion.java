9bff5b3f43e7a03ffe41d0e6e01f602fff011ebe
Sofi799 <sofia.agustina.justiniano@gmail.com>
2022-06-08 23:28:07 -0300

    cambios bla

--- src/ar/edu/unq/po2/tpFinal/Observer/Organizacion.java
+++ src/ar/edu/unq/po2/tpFinal/Observer/Organizacion.java
@@ -5,23 +5,40 @@ import ar.edu.unq.po2.tpFinal.Enumerativos.tipoDeOrganizacion;
 import ar.edu.unq.po2.tpFinal.Ubicaciones.Ubicacion;
 import ar.edu.unq.po2.tpFinal.Ubicaciones.ZonaDeCobertura;
 
-public class Organizacion implements IOrganizacionObserver{
+public class Organizacion implements IOrganizacionObserver {
 
 	private Ubicacion ubicacion;
 	private tipoDeOrganizacion tipo;
 	private Integer cantidadDePersonas;
 
+	public Organizacion(Ubicacion ubicacion, tipoDeOrganizacion tipo, Integer cantidadPersonas) {
+		this.ubicacion = ubicacion;
+		this.tipo = tipo;
+		this.cantidadDePersonas = cantidadPersonas;
+	}
+
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public tipoDeOrganizacion getTipo() {
+		return tipo;
+	}
+
+	public Integer getCantidadDePersonas() {
+		return cantidadDePersonas;
+	}
+
 	@Override
 	public void nuevoEvento(Organizacion organizacion, ZonaDeCobertura zona, Muestra muestra) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	
+
 }
---
+++ test/ar/edu/unq/po2/tpFinal/OrganizacionTestCase.java
@@ -0,0 +1,40 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tpFinal.Enumerativos.tipoDeOrganizacion;
+import ar.edu.unq.po2.tpFinal.Observer.Organizacion;
+import ar.edu.unq.po2.tpFinal.Ubicaciones.Ubicacion;
+import ar.edu.unq.po2.tpFinal.Ubicaciones.ZonaDeCobertura;
+
+class OrganizacionTestCase {
+	
+	Organizacion organizacion;
+	ZonaDeCobertura zona1;
+	ZonaDeCobertura zona2;
+	ZonaDeCobertura zona3;
+	Ubicacion ubicacion;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		ubicacion = mock(Ubicacion.class);
+		zona1 = mock(ZonaDeCobertura.class);
+		zona2 = mock(ZonaDeCobertura.class);
+		zona3 = mock(ZonaDeCobertura.class);
+		organizacion = new Organizacion(ubicacion, tipoDeOrganizacion.SALUD, 5);
+	}
+
+	@Test
+	void testConstructorOrganizacion() {
+		assertEquals(organizacion.getCantidadDePersonas(), 5);
+		assertEquals(organizacion.getTipo(), tipoDeOrganizacion.SALUD);
+		assertEquals(organizacion.getUbicacion(), ubicacion);
+	}
+	
+	
+
+}
