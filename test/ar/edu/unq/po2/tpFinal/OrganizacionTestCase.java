5c81fc762a1838857b913a031d5e221ffdc18ea4
athan <elnahue.-@live.com.ar>
2022-06-09 02:39:50 -0300

    Organizacion test

--- test/ar/edu/unq/po2/tpFinal/OrganizacionTestCase.java
+++ test/ar/edu/unq/po2/tpFinal/OrganizacionTestCase.java
@@ -2,6 +2,8 @@ package ar.edu.unq.po2.tpFinal;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,13 +23,17 @@ class OrganizacionTestCase {
 	Ubicacion ubicacion;
 	IFuncionalidadExterna nuevaMuestra;
 	IFuncionalidadExterna verificacionMuestra;
-
+	Muestra muestra;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		muestra = mock(Muestra.class);
 		ubicacion = mock(Ubicacion.class);
 		zona1 = mock(ZonaDeCobertura.class);
 		zona2 = mock(ZonaDeCobertura.class);
 		zona3 = mock(ZonaDeCobertura.class);
+		nuevaMuestra = mock(IFuncionalidadExterna.class);
+		verificacionMuestra = mock(IFuncionalidadExterna.class);
 		organizacion = new Organizacion(ubicacion, tipoDeOrganizacion.SALUD, 5, nuevaMuestra, verificacionMuestra);
 	}
 
@@ -36,8 +42,25 @@ class OrganizacionTestCase {
 		assertEquals(organizacion.getCantidadDePersonas(), 5);
 		assertEquals(organizacion.getTipo(), tipoDeOrganizacion.SALUD);
 		assertEquals(organizacion.getUbicacion(), ubicacion);
+		assertEquals(organizacion.getNuevaMuestra(), nuevaMuestra);
+		assertEquals(organizacion.getVerificacionMuestra(), verificacionMuestra);
 	}
 	
+	@Test
+	void testOrganizacionNotificadaConNuevaMuestra() {
+	
+		organizacion.nuevaMuestra(zona1, muestra);
+		
+		verify(nuevaMuestra, times(1)).nuevoEvento(organizacion, zona1, muestra);
+	}
+	
+	@Test
+	void testOrganizacionNotificadaConNuevaVerificacion() {
+		
+		organizacion.nuevaVerificacion(zona1, muestra);
+		
+		verify(verificacionMuestra, times(1)).nuevoEvento(organizacion, zona1, muestra);
+	}
 	
 	
 	
